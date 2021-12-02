5c7f4318fd19e4de47ff1b883d85fbadc8eb8813
Suhas S Reddy <suhasreddy147@gmail.com>
2021-12-02 19:11:49 +0530

    Added Navigation Drawer and Top App Bar in DashboardActivity

--- app/src/main/java/com/example/expensetracker/DashboardActivity.java
+++ app/src/main/java/com/example/expensetracker/DashboardActivity.java
@@ -1,18 +1,63 @@
 package com.example.expensetracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    TextView textView;
+    MaterialToolbar topAppBar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        topAppBar = findViewById(R.id.topAppBar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id= item.getItemId();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch(id) {
+                    case R.id.item_settings: {
+                        //Write Code here to navigate to settings page
+                        break;
+                    }
+                    case R.id.item_help_feedback: {
+                        //Write code here for help and feedback
+                        break;
+                    }
+                    case R.id.item_logout:{
+                        //Write code here for logout
+                        break;
+                    }
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
     }
 }
